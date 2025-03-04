21cbd5f9668b247fc889c4e43eee7c86d836afb2
James A. Duncan <james@kinburnstreet.consulting>
2025-03-04 08:56:41 +0000

    Seems to be working now, although I've changed the interface.

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ This little module allows you to subscribe to selectors in your document, so if
 the situation occurs that a DOM element appears that matches your selector subscription,
 then your code gets executed.
 
-      import registerSelector as "http://path.to/module/index.mjs";
+      import SelectorSubscriber as "http://path.to/module/index.mjs";
 
-      registerSelector('p', ( aParagraphElement ) => {
+      SelectorSubscriber.subscribe('p', ( aParagraphElement ) => {
           /* ... gets called for every paragraph element in your document, now or in the future ... */
       });
 
--- index.mjs
+++ index.mjs
@@ -1,54 +1,59 @@
 self._selectorReg = {};
 
-function registerSelector ( selector, callback ) {
-    const registry = self._selectorReg;
-    if ( registry[ selector ] ) {
-        registry[ selector ].push( callback );
-    } else {
-        registry[ selector ] = [ callback ];
-    }
-};
+class SelectorSubscriber {
 
-document.addEventListener('DOMContentLoaded', async() => {
-    const registry = self._selectorReg;    
-
-    const customElements = Array.from( document.getElementsByTagName('*') ).filter( (e) => {        
-        return !!self.customElements.get(e.nodeName.toLowerCase());
-    });
-    const shadows = customElements.map( e => e.shadowRoot );
-
-    const selectors = Object.keys( self._selectorReg );
-    for ( const selector of selectors ) {
-        const nodes = Array.from( [document, ...shadows].map( e => Array.from( e.querySelectorAll( selector ) ) ) ).flat();
-        for ( const node of nodes ) {
-            for ( const cb of registry[ selector ] ) {
-                cb( node, selector );
-            }      
-        }    
-    }
-    
-    const observer = new MutationObserver( function( mutationList, observer ) {
-        const registry  = self._selectorReg;
-        const selectors = Object.keys( self._selectorReg );
-        for ( const mutation of mutationList ) {
-            if ( mutation.addedNodes.length > 0 ) {
-                mutation.addedNodes.forEach( (node) => {
-                    for ( const selector of selectors ) {
-                        if ( !node.querySelectorAll ) break;
-                        const nodes = node.querySelectorAll( selector );                        
-                        for ( const cbnode of nodes ) {
-                            for ( const cb of registry[ selector ] ) {
-                                cb( cbnode, selector );
-                            }      
-                        }	     
-                    }
-                });
+    static {
+        document.addEventListener('DOMContentLoaded', async() => {
+            const registry = self._selectorReg;    
+        
+            const customElements = Array.from( document.getElementsByTagName('*') ).filter( (e) => {        
+                return !!self.customElements.get(e.nodeName.toLowerCase());
+            });
+            const shadows = customElements.map( e => e.shadowRoot );
+        
+            const selectors = Object.keys( self._selectorReg );
+            for ( const selector of selectors ) {
+                const nodes = Array.from( [document, ...shadows].map( e => Array.from( e.querySelectorAll( selector ) ) ) ).flat();
+                for ( const node of nodes ) {
+                    for ( const cb of registry[ selector ] ) {
+                        cb( node, selector );
+                    }      
+                }    
             }
+            
+            const observer = new MutationObserver( function( mutationList, observer ) {
+                const registry  = self._selectorReg;
+                const selectors = Object.keys( self._selectorReg );
+                for ( const mutation of mutationList ) {
+                    if ( mutation.addedNodes.length > 0 ) {
+                        mutation.addedNodes.forEach( (node) => {
+                            for ( const selector of selectors ) {
+                                if (node.matches( selector )) {
+                                    for ( const cb of registry[ selector ] ) {
+                                        cb( node, selector );
+                                    }      
+                                }
+                            }
+                        });
+                    }
+                }
+            });
+            
+            observer.observe(document.querySelector('body').parentNode, { childList: true, subtree: true });
+        });        
+    }
+
+    static subscribe ( selector, callback ) {
+        const registry = self._selectorReg;
+        if ( registry[ selector ] ) {
+            registry[ selector ].push( callback );
+        } else {
+            registry[ selector ] = [ callback ];
         }
-    });
-    
-    observer.observe(document.querySelector('body').parentNode, { childList: true, subtree: true });
-});
+    };    
+
+}
 
 
-export default registerSelector;
+
+export default SelectorSubscriber;
